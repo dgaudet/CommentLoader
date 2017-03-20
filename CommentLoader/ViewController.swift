7469a5eb491daa26e670ac38072e203c7a3309f1
Dean <dean@deangaudet.com>
2017-03-19 20:25:25 -0600

    - ViewController adding tableView and NavigationBar to view controller -- populating it with the data from the CommentTableViewDataSource -- adding ability to save times from an alert view

--- CommentLoader/ViewController.swift
+++ CommentLoader/ViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var navigationBar: UINavigationBar!
+    
+    lazy var dataSource: CommentTableViewDataSource = {
+        return CommentTableViewDataSource(tableView: self.tableView, results: [])
+    }()
+    
+    lazy var dataProvider: RPCommentDataProvider = {
+        return RPCommentDataProvider(delegate: self.dataSource)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.addComment(comment: "Test comment1")
-        self.addComment(comment: "Test comment2")
-        self.addComment(comment: "Test comment3")
         
-        storageManager.getAll { result in
-            switch result {
-            case .Success(let results): print("success \(results)")
-            case .Failure(let error): print("\(error.description)")
-            }
-        }
+        self.tableView.register(CommentCell.self, forCellReuseIdentifier: CommentCell.reuseIdentifier)
+        let navigationItem = UINavigationItem(title: "Comments")
+        let rightBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.addCommentTapped))
+        navigationItem.rightBarButtonItem = rightBarButton
+        self.navigationBar.setItems([navigationItem], animated: false)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        dataProvider.fetchAll()
+        
+        self.saveComment(comment: "Test comment1")
+        self.saveComment(comment: "Test comment2")
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,18 +42,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    let storageManager = RPCommentCachePersistanceManager.sharedInstance
-
-    func addComment(comment: String){
-        let comment = RPComment(comment: comment)
-        
-        storageManager.save(comment) { result in
-            switch result {
-                case .Success(let savedComment): print("saved \(savedComment.description)")
-                case .Failure(let error): print("Error while saving - \(error)")
-            }
+    func addCommentTapped(){
+        presentSaveCommentAlertController { comment in
+            self.saveComment(comment: comment)
         }
     }
-
+    
+    fileprivate func presentSaveCommentAlertController(_ completion: @escaping(String) -> Void) {
+        let alertController = UIAlertController(title: "Add Comment", message: nil, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "New Comment"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { UIAlertAction in
+            guard let title = alertController.textFields?.first?.text else {
+                completion("")
+                return
+            }
+            
+            completion(title)
+        }
+        alertController.addAction(saveAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    fileprivate func saveComment(comment: String){
+        let rpComment = RPComment(comment: comment)
+        
+        dataProvider.save(comment: rpComment)
+    }
 }
-
